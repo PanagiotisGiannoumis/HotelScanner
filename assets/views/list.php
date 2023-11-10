84aa44f3436a8c382b3a2674359e24b2dcb266f1
PanagiotisGiannoumis <giannoumis.panagiotis@gmail.com>
2023-11-10 20:05:06 +0200

    changed the room list

--- assets/views/list.php
+++ assets/views/list.php
@@ -89,14 +89,14 @@
                     <div class="article-description">
                         <img src="../img/rooms/room-1.jpg" alt="">
                         <div>
-                            <h2>HILTON HOTEL<br><span>ATHENS, ZWGRAFOU</span></h2>
-                            <p>This is Hilton Hotel dsdas dsajdbsa d sajdbsa d sajdnsa dsad sajdnsa dsad sad sad as</p>
+                            <h2>Megali Vretania Hotel<br><span> Athens, Syntagma</span></h2>
+                            <p>With a prime location, opposite Constitution Square and the House of Parliament, the Grande Bretagne features luxurious rooms and stunning city views from its magnificent rooftop terrace. WiFi is offered in all rooms and areas.</p>
                             <a href="../views/room.php">Go to Room Page</a>
                         </div>
                     </div>
                     <div class="article-details">
                         <div>
-                            <p>Per Night: 350&euro;</p>
+                            <p>Per Night: 750&euro;</p>
                         </div>
                         <div>
                             <p>Count of Guests: 1</p>
@@ -108,8 +108,8 @@
                     <div class="article-description">
                         <img src="../img/rooms/room-2.jpg" alt="">
                         <div>
-                            <h2>HILTON HOTEL<br><span>ATHENS, ZWGRAFOU</span></h2>
-                            <p>This is Hilton Hotel dsdas dsajdbsa d sajdbsa d sajdnsa dsad sajdnsa dsad sad sad as</p>
+                            <h2>Hilton Hotel<br><span>Athens, Evangelismos</span></h2>
+                            <p>Located in the heart of Athens, the Hilton Athens features elegant rooms, 3 swimming pools, the popular rooftop Galaxy Bar and 4 restaurants. Evangelismos Metro Station is just a few steps away and connects to Syntagma Square within 5 minutes.</p>
                             <a href="../views/room.php">Go to Room Page</a>
                         </div>
                     </div>
@@ -127,14 +127,14 @@
                     <div class="article-description">
                         <img src="../img/rooms/room-3.jpg" alt="">
                         <div>
-                            <h2>HILTON HOTEL<br><span>ATHENS, ZWGRAFOU</span></h2>
-                            <p>This is Hilton Hotel dsdas dsajdbsa d sajdbsa d sajdnsa dsad sajdnsa dsad sad sad as</p>
+                            <h2>Perianth Hotel<br><span>Athens, Monastiraki</span></h2>
+                            <p>The curved balconies of this Bauhaus landmark overlook the lively café scene on Agia Irini Square, named after the domed church that anchors the plaza.</p>
                             <a href="../views/room.php">Go to Room Page</a>
                         </div>
                     </div>
                     <div class="article-details">
                         <div>
-                            <p>Per Night: 350&euro;</p>
+                            <p>Per Night: 560&euro;</p>
                         </div>
                         <div>
                             <p>Count of Guests: 1</p>
@@ -146,14 +146,14 @@
                     <div class="article-description">
                         <img src="../img/rooms/room-4.jpg" alt="">
                         <div>
-                            <h2>HILTON HOTEL<br><span>ATHENS, ZWGRAFOU</span></h2>
-                            <p>This is Hilton Hotel dsdas dsajdbsa d sajdbsa d sajdnsa dsad sajdnsa dsad sad sad as</p>
+                            <h2>Shila Hotel<br><span>Athens, Kolonaki</span></h2>
+                            <p>You could walk past this residence on a pedestrian street in upmarket Kolonaki and never know it was the city’s most soulful guesthouse. </p>
                             <a href="../views/room.php">Go to Room Page</a>
                         </div>
                     </div>
                     <div class="article-details">
                         <div>
-                            <p>Per Night: 350&euro;</p>
+                            <p>Per Night: 400&euro;</p>
                         </div>
                         <div>
                             <p>Count of Guests: 1</p>
@@ -165,14 +165,14 @@
                     <div class="article-description">
                         <img src="../img/rooms/room-5.jpg" alt="">
                         <div>
-                            <h2>HILTON HOTEL<br><span>ATHENS, ZWGRAFOU</span></h2>
-                            <p>This is Hilton Hotel dsdas dsajdbsa d sajdbsa d sajdnsa dsad sajdnsa dsad sad sad as</p>
+                            <h2>Four Seasons Astir Palace<br><span>Athens, Vouliagmeni</span></h2>
+                            <p>The Astir Palace put the Athens Riviera on the map in the 1960s. After a recent glossy makeover courtesy of the Four Seasons, it’s back—and exactly what this up-and-coming coastline needed: a full-blown seaside hotel that isn’t stuck in the past. </p>
                             <a href="../views/room.php">Go to Room Page</a>
                         </div>
                     </div>
                     <div class="article-details">
                         <div>
-                            <p>Per Night: 350&euro;</p>
+                            <p>Per Night: 850&euro;</p>
                         </div>
                         <div>
                             <p>Count of Guests: 1</p>
